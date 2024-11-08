d43cd83c39ebbc7b1b35b7e29354faab04000ebc
Unknown-903 <shubhamsinghal05072008@gmail.com>
2024-11-08 17:19:39 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -26,8 +26,8 @@ async def Cb_Handle(bot: Client, query: CallbackQuery):
         btn = [
             [InlineKeyboardButton(text='❗ Hᴇʟᴘ', callback_data='help'), InlineKeyboardButton(
                 text='🌨️ Aʙᴏᴜᴛ', callback_data='about')],
-            [InlineKeyboardButton(text='📢 Uᴘᴅᴀᴛᴇs', url='https://t.me/powerleech'), InlineKeyboardButton
-                (text='💻 Dᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/powerleech')]
+            [InlineKeyboardButton(text='📢 Uᴘᴅᴀᴛᴇs', url='https://t.me/Officialcreater'), InlineKeyboardButton
+                (text='💻 Dᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/Officialcreater')]
         ]
         await query.message.edit(text=Txt.PRIVATE_START_MSG.format(query.from_user.mention), reply_markup=InlineKeyboardMarkup(btn))
 
